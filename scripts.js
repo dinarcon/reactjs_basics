3d6a10accc510589d5ab079617c5013420ba5a61
Mauricio Dinarte <drupal@dinarcon.com>
2019-03-09 10:05:17 -0600

    Create movie component

--- scripts.js
+++ scripts.js
@@ -9,35 +9,27 @@ function Header() {
   );
 }
 
+function Movie() {
+  return (
+    <article>
+      <h3>Space Jam</h3>
+      <div className="year">Publicada en 1996</div>
+      <img
+        alt="Portada de Space Jam"
+        src="https://m.media-amazon.com/images/M/MV5BMDgyZTI2YmYtZmI4ZC00MzE0LWIxZWYtMWRlZWYxNjliNTJjXkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_SX300.jpg"
+      />
+    </article>
+  );
+}
+
 const vDOM = (
   <React.Fragment>
     <Header />
     <h2>En cartelera</h2>
     <section>
-      <article>
-        <h3>Space Jam</h3>
-        <div className="year">Publicada en 1996</div>
-        <img
-          alt="Portada de Space Jam"
-          src="https://m.media-amazon.com/images/M/MV5BMDgyZTI2YmYtZmI4ZC00MzE0LWIxZWYtMWRlZWYxNjliNTJjXkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_SX300.jpg"
-        />
-      </article>
-      <article>
-        <h3>Muppets from Space</h3>
-        <div className="year">Publicada en 1999</div>
-        <img
-          alt="Portada de Muppets from Space"
-          src="https://m.media-amazon.com/images/M/MV5BODI2Zjc5YzMtNzQ1NS00NGVmLWExYzgtMzFlY2NiMGMzZjRjXkEyXkFqcGdeQXVyNTUyMzE4Mzg@._V1_SX300.jpg"
-        />
-      </article>
-      <article>
-        <h3>2001: A Space Odyssey</h3>
-        <div className="year">Publicada en 1968</div>
-        <img
-          alt="Portada de 2001: A Space Odyssey"
-          src="https://m.media-amazon.com/images/M/MV5BMmNlYzRiNDctZWNhMi00MzI4LThkZTctMTUzMmZkMmFmNThmXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-        />
-      </article>
+      <Movie />
+      <Movie />
+      <Movie />
     </section>
   </React.Fragment>
 );
