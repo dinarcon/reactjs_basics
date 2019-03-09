454cda2de1b1eaea52f647e3bd9077bc0d5abce1
Mauricio Dinarte <drupal@dinarcon.com>
2019-03-09 09:55:45 -0600

    Nested JSX element

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 const vDOM = (
-  <p className="subheading">
-    El mejor <strong>cine al aire libre</strong> de Managua.
-  </p>
+  <header>
+    <h1>Cine de las estrellas</h1>
+    <p className="subheading">
+      El mejor <strong>cine al aire libre</strong> de Managua.
+    </p>
+  </header>
 );
 
 const renderTarget = document.getElementById("app");
