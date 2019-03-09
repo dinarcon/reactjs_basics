6fef2ab6ec925bdd62e0ff604490dc9f51c7167a
Mauricio Dinarte <drupal@dinarcon.com>
2019-03-09 09:32:03 -0600

    Simple react element

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const vDOM = React.createElement(
+  "p",
+  null,
+  "El mejor cine al aire libre de Managua."
+);
+
+const renderTarget = document.getElementById("app");
+ReactDOM.render(vDOM, renderTarget);
