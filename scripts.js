be39da4215284cdd498ae68e6525181359251f0c
Mauricio Dinarte <drupal@dinarcon.com>
2019-03-09 09:58:46 -0600

    First component

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
-const vDOM = (
-  <header>
-    <h1>Cine de las estrellas</h1>
-    <p className="subheading">
-      El mejor <strong>cine al aire libre</strong> de Managua.
-    </p>
-  </header>
-);
+function Header() {
+  return (
+    <header>
+      <h1>Cine de las estrellas</h1>
+      <p className="subheading">
+        El mejor <strong>cine al aire libre</strong> de Managua.
+      </p>
+    </header>
+  );
+}
+
+const vDOM = <Header />;
 
 const renderTarget = document.getElementById("app");
 ReactDOM.render(vDOM, renderTarget);
