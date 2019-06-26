d3c95a4cd2dcaa9f88fe67d4b890a6d00fe1c65e
Mauricio Dinarte <drupal@dinarcon.com>
2019-06-26 07:07:00 -0600

    Add exercises

---
+++ exercises/01-react-elements/scripts.js
@@ -0,0 +1,8 @@
+// TODO 1. Create React element.
+// Use React.createElement()
+
+// TODO 2. Get reference to DOM mounting point.
+// Use document.getElementById() or document.querySelector()
+
+// TODO 3. Render element into DOM mounting point.
+// Use ReactDOM.render()
