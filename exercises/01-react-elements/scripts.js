97eab83120e7c1a2fb1f072d692e5a018252a8a8
Mauricio Dinarte <drupal@dinarcon.com>
2020-06-25 07:07:03 -0600

    Remove exercises from class-components branch

--- exercises/01-react-elements/scripts.js
+++
@@ -1,8 +0,0 @@
-// TODO 1. Create React element.
-// Use React.createElement()
-
-// TODO 2. Get reference to DOM mounting point.
-// Use document.getElementById() or document.querySelector()
-
-// TODO 3. Render element into DOM mounting point.
-// Use ReactDOM.render()
