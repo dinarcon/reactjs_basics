b85e55b16ac6881cf7485744f8d0aba078a26286
Mauricio Dinarte <drupal@dinarcon.com>
2020-06-25 08:08:19 -0600

    Create simple React element

---
+++ exercises/01-simple-react-element/script.js
@@ -0,0 +1,8 @@
+// TODO 1: Create a React element.
+// Use React.createElement()
+
+// TODO 2: Get reference to DOM's container.
+// Use document.getElementById() or document.querySelector()
+
+// TODO 3: Render the element into DOM's container.
+// Use ReactDOM.render()
--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-// TODO: Implement React application.
+const element = React.createElement("p", null, "Vote for your favorite one.");
+const domContainer = document.getElementById("react-app");
+ReactDOM.render(element, domContainer);
